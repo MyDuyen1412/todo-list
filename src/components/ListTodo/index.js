ff1eaee6455523f583aac36b7c753ca4c8a80d01
Duyen Nguyen <duyen.nguyen@2359media.com>
2019-12-10 10:32:18 +0700

    thêm bậy bạ

--- src/components/ListTodo/index.js
+++ src/components/ListTodo/index.js
@@ -1,16 +1,29 @@
 import _ from "lodash";
 import classnames from "classnames";
-import React, { useContext } from "react";
+import React, { useContext, useRef, useEffect } from "react";
 import Context from "../../context/Context.js";
 import { useHistory } from "react-router-dom";
-import { setTodoList, getTodoPin, getTodoNotPin } from "../../utils/datasource.js";
+import {
+  setTodoList,
+  getTodoPin,
+  getTodoNotPin
+} from "../../utils/datasource.js";
 import Todo from "./Todo";
 import Masonry from "react-masonry-component";
+// import {
+//   GridContextProvider,
+//   GridDropZone,
+//   GridItem,
+//   swap,
+//   move
+// } from "react-grid-dnd";
+import Muuri from "muuri";
 import styles from "./styles.module.css";
 
 function ListTodo() {
   const { todos, setTodos } = useContext(Context);
   const history = useHistory();
+  const ref = useRef();
 
   const handleDelete = id => {
     setTodoList(_.omit(todos, id));
@@ -42,9 +55,65 @@ function ListTodo() {
     .reverse()
     .filter(item => item.pin);
 
+  // useEffect(() => {
+  //   Muuri.defaultOptions.dragEnabled = true;
+  //   Muuri.defaultOptions.layoutDuration =  600
+  //   new Muuri(ref.current);
+  // }, [todos]);
+
+  // const onChange = (sourceId, sourceIndex, targetIndex, targetId) => {
+  //   const nextState = swap(
+  //     Object.values(todos),
+  //     sourceIndex,
+  //     targetIndex
+  //   );
+  //   var obj = nextState.reduce(function(acc, cur, i) {
+  //     acc[i] = cur;
+  //     return acc;
+  //   }, {});
+  //   setTodos(obj);
+  //   // setItems(nextState);
+  // };
+
+  // console.log(todos);
+
   return (
+    // <GridContextProvider onChange={onChange}>
+    //   <GridDropZone
+    //     id="items"
+    //     boxesPerRow={4}
+    //     rowHeight={100}
+    //     style={{ height: "400px" }}
+    //   >
+    //     {Object.values(todos)
+    //       .reverse()
+    //       .map(item => (
+    //         <GridItem key={item.id}>
+    //           <div
+    //             style={{
+    //               width: "100%",
+    //               // height: "100%"
+    //             }}
+    //           >
+    //             <Todo item={item} open={openItem} handleDelete={handleDelete} />
+    //           </div>
+    //         </GridItem>
+    //       ))}
+    //   </GridDropZone>
+    // </GridContextProvider>
     <>
-    {/* <Masonry className={styles.list}>
+      {/* <div className={styles.grid} ref={ref}>
+        {Object.values(todos)
+          .reverse()
+          .map(item => (
+            <div key={item.id} className={classnames(styles.container,styles.item)}>
+              <div className={styles["item-content"]}>
+                <Todo item={item} open={openItem} handleDelete={handleDelete} handlePin={handlePin}/>
+              </div>
+            </div>
+          ))}
+      </div> */}
+      {/* <Masonry className={styles.list}>
         {Object.values(todos)
           .reverse()
           .map(item => (
@@ -53,6 +122,7 @@ function ListTodo() {
                 item={item}
                 open={openItem}
                 handleDelete={handleDelete}
+                handlePin={handlePin}
               />
             </div>
           ))}
